6cd1926e4158a687993ef79871c6df4173edc2e1
Mike Semko <msemko@gmail.com>
2016-09-27 13:50:49 -0400

    Added error middleware for 404 and 500 pages, added appropriate views

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.set("view engine", "ejs");
 
 require("./controllers")(app);
 
+app.use((req, res)=>{
+  res.status(404).render("common/404");
+});
+
+app.use((err, req, res, next)=>{
+  if (app.get("env") === 'development') {
+    res.status(500).json(err);
+  } else {
+    res.status(500).render("common/500");
+  }
+  next();
+});
+
 let port = process.env.NODE_PORT || 3000;
 
 app.listen(port, ()=>{ console.log(`Server running on port ${port}`); });
