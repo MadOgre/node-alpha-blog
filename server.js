c75ba06cd5027c8a6b058138d47082041f556466
Mike Semko <msemko@gmail.com>
2016-09-27 02:46:31 -0400

    added view engine and set up layout files

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 let express = require("express");
 let app = express();
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 let port = process.env.NODE_PORT || 3000;
 
 app.listen(port, ()=>{ console.log(`Server running on port ${port}`); });
