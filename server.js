ec8ca822d8eb41dd3c578b8084a410f82345c0cb
Mike Semko <msemko@gmail.com>
2016-09-27 02:54:18 -0400

    added static pages controller

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ let app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+require("./controllers")(app);
+
 let port = process.env.NODE_PORT || 3000;
 
 app.listen(port, ()=>{ console.log(`Server running on port ${port}`); });
