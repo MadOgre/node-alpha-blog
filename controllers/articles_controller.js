d7ae15a4ed6e8d7bfa8e4b057f9fa9f36d3134a9
Mike Semko <msemko@gmail.com>
2016-09-28 17:34:50 -0400

    installed body-parser, added post route for article

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -15,4 +15,11 @@ router.get("/new", (req, res)=>{
   res.render("articles/new");
 });
 
+router.post("/new", (req, res, next)=>{
+  Article.create(req.body, (err, data)=>{
+    if (err) return next(err);
+    res.redirect("/articles");
+  });
+});
+
 module.exports = router;
