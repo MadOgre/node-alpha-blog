bf1b843d0e30cae5d87671a676b5bc4d7654f2f1
Mike Semko <msemko@gmail.com>
2016-09-28 17:18:05 -0400

    added slugs to schema

--- models/article.js
+++ models/article.js
@@ -1,11 +1,14 @@
 "use strict";
 let mongoose = require("mongoose");
+let URLSlugs = require("mongoose-url-slugs");
 
 let ArticleSchema = new mongoose.Schema({
   title: String,
   author: String,
   body: String
 });
+
+ArticleSchema.plugin(URLSlugs('title'));
 
 let Article = mongoose.model("Article", ArticleSchema);
 
