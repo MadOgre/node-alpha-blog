0bf66b67e36a78f2c3aafc47c3fae9b2ca1b4d6f
Mike Semko <msemko@gmail.com>
2016-09-28 01:01:11 -0400

    Updated views to nest content inside bootstrap container, added index, and show routes for articles controller

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -9,7 +9,39 @@ router.get("/", (req, res, next)=>{
     if (err) {
       return next(err);
     } else {
-      res.json(data);
+      res.render("articles/index", {articles: data});
+    }
+  });
+});
+
+router.get("/:slug_or_id", (req, res, next)=>{
+  Article.findOne({slug: req.params.slug_or_id}, (err, data)=>{
+    if (err) {
+      return next(err);
+    }
+    if (data) {
+      return res.render("articles/show", {article: data});
+    }
+    if (req.params.slug_or_id.match(/^[0-9a-fA-F]{24}$/)) {
+      Article.findById(req.params.slug_or_id, (err, data)=>{
+        if (err) {
+          return next(err);
+        }
+        if (data) {
+          return res.render("articles/show", {article: data});
+        }
+      });
+    }
+    return next();
+  });
+});
+
+router.post("/", (req, res, next)=>{
+  Article.create(req.body, (err, data)=>{
+    if (err) {
+      return next(err);
+    } else {
+      res.redirect
     }
   });
 });
